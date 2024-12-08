e7c2bd9374058079a4bfa683c5ba147bae2f989e
AnkitJhadev <jha80250@gmail.com>
2024-12-08 18:01:06 +0530

    "Initialize Material-UI and set up basic routing in the app"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
+import Header from './Components/Header'
+import Home from './pages/Home'
+import CoinPage from './pages/CoinPage'
 function App() {
   
 
   return (
-    <>
-          
-    </>
+    <BrowserRouter>
+     <div>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coin/:id' element={<CoinPage/>}/>
+    </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
